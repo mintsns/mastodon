387c07ef91f8d9e9a32af5a650430682401a81f1
nacika <nacika.inscatolare@gmail.com>
2018-10-14 08:10:24 +0900

    2018/10/14

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,5 +2,7 @@ COMPOSE_HTTP_TIMEOUT=9999
 docker-compose build && \
 docker-compose run --rm web rails db:migrate && \
 # docker-compose run --rm web rails mastodon:maintenance:prepare_for_foreign_keys && \
-docker-compose run --rm web sh -c 'npm upgrade --global yarn' && \
-docker-compose run --rm web rake assets:precompile
+# docker-compose run --rm web sh -c 'npm upgrade --global yarn' && \
+docker-compose run --rm web rake assets:precompile && \
+docker-compose stop && \
+docker-compose up -d
